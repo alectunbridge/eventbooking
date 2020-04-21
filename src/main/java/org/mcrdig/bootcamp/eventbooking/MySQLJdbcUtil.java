26969000b4073fb6c69be75b575b62ae5b6308cc
sztanderap <sztandera.p@gmail.com>
2020-04-21 14:32:29 +0100

    Add getUsers method

--- src/main/java/org/mcrdig/bootcamp/eventbooking/MySQLJdbcUtil.java
+++ src/main/java/org/mcrdig/bootcamp/eventbooking/MySQLJdbcUtil.java
@@ -3,9 +3,12 @@ package org.mcrdig.bootcamp.eventbooking;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,7 +17,8 @@ public class MySQLJdbcUtil {
     public static void main(String[] args) throws SQLException {
 
         MySQLJdbcUtil mySQLJdbcUtil = new MySQLJdbcUtil();
-        mySQLJdbcUtil.createUser("Julie");
+        // mySQLJdbcUtil.createUser("Julie");
+        System.out.println( mySQLJdbcUtil.getUsers());
     }
 
     public void createUser(String userName) throws SQLException {
@@ -35,5 +39,37 @@ public class MySQLJdbcUtil {
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
         }
+    }
+
+    public List<User> getUsers() throws SQLException {
+
+        List<User> users = new ArrayList<>();
+
+        try (Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://db-workshop.public-dev.zuto.cloud:3306/bootcamp3?user=bootcamp3&password=TelephoneWeek"))
+        {
+            conn.setAutoCommit(false);
+
+            String query = "select * from user ";
+            Statement statement = conn.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while (resultSet.next()) {
+                User user = new User(resultSet.getInt("id"), resultSet.getString("name"));
+                users.add(user);
+            }
+
+            conn.commit();
+
+
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+
+        return users;
     }
 }
